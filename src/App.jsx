c61d2aaacb285d3b82d1cc27812ca9311d04868a
Kauanchocair <kchocair@gmail.com>
2024-05-15 11:02:53 -0300

    Projeto1505

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+ 
+const App = () => (
+  <>
+    <div id='root'>
+      <section>
+        <header className="header">
+          AULA REACT
+        </header>
+        <div id='conteudo'>
+          AULA INICIAL DE REACTJS
+        </div>
+        <footer className="footer">
+          RETOMADA
+        </footer>
+      </section>
+    </div>
+  </>
+);
+ 
+export default App;
